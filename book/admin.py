d07e50b52884e16b98e0e16274e1d47822b09019
mramirhosein <mramirhoseinroshan@gmail.com>
2018-08-19 01:36:52 -0700

    change gitignore create models and first views

--- book/admin.py
+++ book/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Genre,Language,Author,Book,BookInstance
 # Register your models here.
+
+admin.site.register(Genre)
+admin.site.register(Language)
+admin.site.register(Author)
+admin.site.register(Book)
+admin.site.register(BookInstance)
